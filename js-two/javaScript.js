8f4df8b5bbc958791692e58d5d9220247a656b02
Abdurazzoqov Shohrux <qtowkentboyev@gmail.com>
2023-01-16 17:58:43 +0500

    first commit

---
+++ js-two/javaScript.js
@@ -0,0 +1,11 @@
+let namePrompt = prompt ("What is your name?")
+let surnamePrompt = prompt ("What is your surname?")
+let agePrompt = prompt ("How old are you?")
+
+let user = {
+    name: namePrompt,
+    surname: surnamePrompt,
+    age: agePrompt
+};
+
+console.log = (user)
